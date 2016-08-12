00b1da52129c34f5eb2d32e41d3307c44de9b124
陈慕杰 <642163903@qq.com>
2016-08-12 20:40:55 +0800

    增加list, 和component.html部分响应式

--- src/javascripts/component.js
+++ src/javascripts/component.js
@@ -10,8 +10,10 @@ let btn2 = document.getElementById('closeCollapse2');
 let btnAll = document.getElementById('closeCollapseAll');
 let btn = document.getElementById('closeCollapseSingle');
 let blueCat = document.getElementById('blueCat');
-
+let menuContentBtn = document.querySelector('.menu-content-btn');
+console.log(menuContentBtn);
 btn1.addEventListener('click', () => closeGroup('group1'));
 btn2.addEventListener('click',() => closeGroup('group2'));
 btnAll.addEventListener('click', () => slideAll('close'));
 btn.addEventListener('click', blueCat.slideCollapse);
+menuContentBtn.addEventListener('click', () => document.querySelector('.menu-content').classList.toggle('on'))
